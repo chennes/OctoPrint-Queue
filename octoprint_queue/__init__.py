4505b646e1f165b8aa056c1b5108900b77953b21
Chris Hennes <chennes@pioneerlibrarysystem.org>
2018-12-28 12:59:13 -0500

    Initial commit.

---
+++ octoprint_queue/__init__.py
@@ -0,0 +1,243 @@
+# coding=utf-8
+from __future__ import absolute_import
+
+# A lot of this code is based on Jarek Szczepanski's Print History plugin. Thanks, Jarek!
+
+__author__ = "Chris Hennes <chennes@pioneerlibrarysystem.org"
+__license__ = "GNU Affero General Public License http://www.gnu.org/licenses/agpl.html"
+__copyright__ = "Copyright (C) 2018 Pioneer Library System. Released under the terms of the AGPLv3 license."
+
+from flask import jsonify, request, make_response
+from octoprint.server.util.flask import with_revalidation_checking, check_etag
+
+import octoprint.plugin
+import sqlite3
+import os
+
+class QueuePlugin(octoprint.plugin.StartupPlugin,
+				  octoprint.plugin.SettingsPlugin,
+				  octoprint.plugin.AssetPlugin,
+				  octoprint.plugin.TemplatePlugin,
+				  octoprint.plugin.BlueprintPlugin,
+				  octoprint.plugin.EventHandlerPlugin):
+
+	def on_after_startup(self):
+		self._queue_dict = None
+		self._queue_db_path = os.path.join(self.get_plugin_data_folder(),"queue.db")
+		connection = sqlite3.connect(self._queue_db_path)
+		cursor = connection.cursor()
+		creation = """\
+CREATE TABLE IF NOT EXISTS queue (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  submissiontimestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  staff TEXT NOT NULL DEFAULT "",
+  customer TEXT NOT NULL DEFAULT "",
+  contact TEXT NOT NULL DEFAULT "",
+  filename TEXT NOT NULL DEFAULT "",
+  note TEXT,
+  printtype INTEGER,
+  cost REAL,
+  prepaid INTEGER,
+  status INTEGER,
+  archived INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS modifications (
+  id INTEGER NOT NULL PRIMARY KEY ON CONFLICT REPLACE,
+  action TEXT NOT NULL,
+  changed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TRIGGER IF NOT EXISTS queue_onupdate AFTER UPDATE ON queue
+BEGIN
+  INSERT INTO modifications (id, action) VALUES (old.id, 'UPDATE');
+END;
+
+CREATE TRIGGER IF NOT EXISTS queue_ondelete AFTER DELETE ON queue
+BEGIN
+  INSERT INTO modifications (id, action) VALUES (old.id, 'DELETE');
+END;
+
+CREATE TRIGGER IF NOT EXISTS queue_oninsert AFTER INSERT ON queue
+BEGIN
+  INSERT INTO modifications (id, action) VALUES (new.id, 'INSERT');
+END;
+
+"""
+		cursor.executescript(creation)
+		connection.close()
+
+	##~~ SettingsPlugin mixin
+
+	def get_settings_defaults(self):
+		return dict(
+			printtypes=["Urgent","Customer","Student","Internal","Other"]
+		)
+
+	##~~ AssetPlugin mixin
+
+	def get_assets(self):
+		return dict(
+			js=["js/queue.js"],
+			css=["css/queue.css"],
+			less=["less/queue.less"]
+		)
+
+	##~~ Softwareupdate hook
+
+	def get_update_information(self):
+		return dict(
+			queue=dict(
+				displayName="Queue Plugin",
+				displayVersion=self._plugin_version,
+
+				# version check: github repository
+				type="github_release",
+				user="chennes",
+				repo="OctoPrint-Queue",
+				current=self._plugin_version,
+
+				# update method: pip
+				pip="https://github.com/chennes/OctoPrint-Queue/archive/{target_version}.zip"
+			)
+		)
+
+	##~~ EventPlugin mixin
+
+	#def on_event(self, event, payload):
+	#	from . import eventHandler
+	#	return eventHandler.eventHandler(self, event, payload)
+
+
+	##~~ Blueprint mixin -- basically the whole plugin
+	@octoprint.plugin.BlueprintPlugin.route("/queue", methods=["GET"])
+	def getQueue(self):
+		from octoprint.settings import valid_boolean_trues
+		force = request.values.get("force","false") in valid_boolean_trues
+		if force:
+			self._queue_dict = None
+		def view():
+			queue_dict = self._getQueueDict()
+			if queue_dict is not None:
+				return jsonify(queue=queue_dict)
+			else:
+				return jsonify({})
+		def etag():
+			# The etag here is used to check the modifications table, which tracks changes to the
+			# queue
+			connection = sqlite3.connect(self._queue_db_path)
+			cursor = connection.cursor()
+			cursor.execute("SELECT changed_at FROM modifications ORDER BY changed_at DESC LIMIT 1");
+			last_modified = cursor.fetchone()
+			connection.close()
+			import hashlib
+			hash = hashlib.sha1()
+			hash.update(str(last_modified))
+			return hash.hexdigest()
+		def condition():
+			return check_etag(etag())
+		
+		return with_revalidation_checking(etag_factory=lambda *args, **kwargs: etag(),
+										  condition=lambda *args, **kwargs: condition(),
+										  unless=lambda: force)(view)()
+
+	@octoprint.plugin.BlueprintPlugin.route("/addtoqueue", methods=["PUT"])
+	def addToQueue(self):
+		from werkzeug.exceptions import BadRequest
+		try:
+			json_data = request.json
+		except BadRequest:
+			return make_response("Malformed JSON body in request", 400)
+
+		staff = json_data["staff"] if "staff" in json_data else ""  
+		customer = json_data["customer"] if "customer" in json_data else ""  
+		contact = json_data["contact"] if "contact" in json_data else ""  
+		filename = json_data["filename"]   if "filename" in json_data else ""  
+		note = json_data["note"]   if "note" in json_data else ""  
+		printtype = json_data["printtype"]   if "printtype" in json_data else 0  
+		cost = json_data["cost"]   if "cost" in json_data else 0.0
+		prepaid = json_data["prepaid"]   if "prepaid" in json_data else 0  
+		status = json_data["status"]   if "status" in json_data else 0
+		archived = json_data["archived"]   if "archived" in json_data else 0  
+		
+		self._queue_dict = None
+
+		connection = sqlite3.connect(self._queue_db_path)
+		cursor = connection.cursor()
+		cursor.execute("INSERT INTO queue (staff, customer, contact, filename, note, printtype, cost, prepaid, status, archived) VALUES (?,?,?,?,?,?,?,?,?,?)",(staff,customer,contact,filename,note,printtype,cost,prepaid,status,archived))
+		connection.commit()
+		connection.close()
+		return self.getQueue()
+
+	@octoprint.plugin.BlueprintPlugin.route("/archive", methods=["PUT"])
+	def archive(self):
+		from werkzeug.exceptions import BadRequest
+		try:
+			json_data = request.json
+		except BadRequest:
+			return make_response("Malformed JSON body in request", 400)
+		if not "id" in json_data:
+			return make_response("No ID in request", 400)
+		itemid = json_data["id"]
+		archived = json_data["archived"] if "archived" in json_data else 1  
+		self._queue_dict = None
+		connection = sqlite3.connect(self._queue_db_path)
+		cursor = connection.cursor()
+		cursor.execute("UPDATE queue SET archived=? WHERE id=?",(archived,itemid))
+		connection.commit()
+		connection.close()
+		return self.getQueue()
+
+	@octoprint.plugin.BlueprintPlugin.route("/modifyitem", methods=["PUT"])
+	def modifyItem(self):
+		from werkzeug.exceptions import BadRequest
+		try:
+			json_data = request.json
+		except BadRequest:
+			return make_response("Malformed JSON body in request", 400)
+		if not "id" in json_data:
+			return make_response("No ID in request", 400)
+		itemid = json_data["id"]
+		staff = json_data["staff"] if "staff" in json_data else ""  
+		customer = json_data["customer"] if "customer" in json_data else ""  
+		contact = json_data["contact"] if "contact" in json_data else ""  
+		filename = json_data["filename"]   if "filename" in json_data else ""  
+		note = json_data["note"]   if "note" in json_data else ""  
+		printtype = json_data["printtype"]   if "printtype" in json_data else 0  
+		cost = json_data["cost"]   if "cost" in json_data else 0.0
+		prepaid = json_data["prepaid"]   if "prepaid" in json_data else 0  
+		status = json_data["status"]   if "status" in json_data else 0
+		archived = json_data["archived"] if "archived" in json_data else 1  
+
+		self._queue_dict = None
+		connection = sqlite3.connect(self._queue_db_path)
+		cursor = connection.cursor()
+		cursor.execute("UPDATE queue SET staff=?,customer=?,contact=?,filename=?,note=?,printtype=?,cost=?,prepaid=?,status=?,archived=? where id=?",(staff,customer,contact,filename,note,printtype,cost,prepaid,status,archived,itemid))
+		connection.commit()
+		connection.close()
+		return self.getQueue()
+
+
+		
+	def _getQueueDict(self):
+		if self._queue_dict is not None:
+			return self._queue_dict
+		connection = sqlite3.connect(self._queue_db_path)
+		cursor = connection.cursor()
+		cursor.execute("SELECT * FROM queue ORDER BY printtype ASC, submissiontimestamp DESC")
+		queue_dict = [dict((cursor.description[i][0], value) for i,value in enumerate(row)) for row in cursor.fetchall()]
+		connection.close()
+		if queue_dict is None:
+			queue_dict = dict()
+		self._queue_dict = queue_dict
+		return self._queue_dict
+
+def __plugin_load__():
+	global __plugin_implementation__
+	__plugin_implementation__ = QueuePlugin()
+
+	global __plugin_hooks__
+	__plugin_hooks__ = {
+		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+	}
+
---
+++ octoprint_queue/static/js/queue.js
@@ -0,0 +1,319 @@
+/*
+ * View model for OctoPrint-Queue
+ *
+ * Author: Chris Hennes
+ * License: AGPLv3 
+ */
+$(function() {
+    function QueueViewModel(parameters) {
+        var self = this;
+        self.loginState = parameters[0];
+        self.global_settings = parameters[1];
+        self.users = parameters[2];
+
+        self.isPrinting = ko.observable(undefined);
+        self.itemForEditing = ko.observable();
+        self.addingNew = ko.observable();
+        self.itemForArchiving = ko.observable();
+
+
+        var QueueItem = function(data) {
+            this.id = ko.observable();
+            this.staff = ko.observable();
+            this.customer = ko.observable();
+            this.contact = ko.observable();
+            this.filename = ko.observable();
+            this.cost = ko.observable();
+            this.note = ko.observable();
+
+            this.archived = ko.observable();
+            this.prepaid = ko.observable();
+            this.printtype = ko.observable();
+            this.submissiontimestamp = ko.observable();
+
+            this.printtypes = self.global_settings.settings.plugins.queue.printtypes;
+
+            this.archivedBool = ko.computed(function() {
+                return this.archived() == 1;
+            }, this);
+            this.prepaidBool = ko.computed(function() {
+                return this.prepaid() == 1;
+            }, this);
+            this.printtypeString = ko.computed(function() {
+                return this.printtypes()[this.printtype()];
+            }, this);
+            this.timeAgo = ko.computed(function() {
+                var ms = Date.parse(this.submissiontimestamp()+"Z");
+                var s = ms / 1000.0;
+                return formatTimeAgo(s);
+            }, this);
+
+            this.update(data);
+        }
+
+        QueueItem.prototype.update = function (data) {
+            var updateData = data || {};
+            this.id (updateData.id || 0);  
+            this.staff (updateData.staff || "");  
+            this.customer (updateData.customer || "");  
+            this.contact (updateData.contact || "");  
+            this.filename (updateData.filename || "");  
+            this.cost (updateData.cost || 0.0);  
+            this.note (updateData.note || "");  
+
+            this.archived (updateData.archived || false);  
+            this.prepaid (updateData.prepaid || false);  
+            this.printtype (updateData.printtype || 0);  
+            this.submissiontimestamp (updateData.submissiontimestamp || 0);
+        }
+
+        self.onQueueTab = false;
+        self.dataIsStale = true;
+        self.requestingData = false;
+        self.pureData = {};
+
+        self.onStartup = function () {
+            self.editDialog = $("#edit_dialog");
+            self.editDialog.on('hidden', self.onCancelEdit);
+            self.archiveDialog = $("#archive_dialog");
+            self.archiveDialog.on('hidden', self.onCancelArchive);
+        }
+
+        self.onBeforeBinding = function () {
+            self.settings = self.global_settings.settings.plugins.queue;
+            self.printtypes = self.settings.printtypes;
+        }
+
+        self.onAfterTabChange = function(current, previous) {
+            self.onQueueTab = current == "#tab_plugin_queue";
+        }
+
+        self.fromCurrentData = function(data) {
+            var isPrinting = data.state.flags.printing;
+
+            if (isPrinting != self.isPrinting()) {
+                self.requestData();
+            }
+            self.isPrinting(isPrinting);
+        }
+
+        self.requestData = function(params) {
+            var force = false;
+            if (_.isObject(params)) {
+                force = params.force;
+            }
+            if (!self.onQueueTab) {
+                self.dataIsStale = true;
+                return;
+            }
+            if (self.requestingData) {
+                return;
+            }
+            self.requestionData = true;
+            $.ajax({
+                url: "plugin/queue/queue",
+                type: "GET",
+                data: {force: force},
+                dataType: "json",
+                success: self.fromResponse
+            }).always(function () {
+                self.requestingData = false;
+            });
+        }
+
+        self.fromResponse = function(data) {
+            var dataRows = ko.utils.arrayMap(data.queue, function (data) {
+                return new QueueItem(data);
+            });
+            self.pureData = data.queue;
+            self.dataIsStale = false;
+            self.listHelper.updateItems(dataRows);
+        }
+
+        self.showArchiveDialog = function(data) {
+            if (self.archiveDialog) {
+                self.itemForArchiving(data);
+                self.archiveDialog.modal("show");
+            }
+        }
+
+        self.toggleArchive = function(data) {
+             var payload = {
+                id: self.itemForArchiving().id(),
+                archived: (self.itemForArchiving().archived()==0?1:0)
+            };
+
+            $.ajax({
+                url: "plugin/queue/archive",
+                type: "PUT",
+                data: JSON.stringify(payload),
+                dataType: "json",
+                contentType: "application/json; charset=UTF-8",
+                success: self.archiveCompleted
+            }).always(function() {/*TODO: Remove the spinner once it exists*/});
+        }
+
+        self.archiveCompleted = function(data) {
+            self.fromResponse(data);
+            self.archiveDialog.modal("hide");
+        }
+
+        self.addToQueue = function(event) {
+            var payload = {
+                staff: self.itemForEditing().staff(),
+                customer: self.itemForEditing().customer(),
+                contact: self.itemForEditing().contact(),
+                filename: self.itemForEditing().filename(),
+                note: self.itemForEditing().note(),
+                cost: self.itemForEditing().cost(),
+                prepaid: self.itemForEditing().prepaid(),
+                printtype: self.printtypes().findIndex(x => x == self.itemForEditing().printtype())
+            };
+
+            $.ajax({
+                url: "plugin/queue/addtoqueue",
+                type: "PUT",
+                data: JSON.stringify(payload),
+                dataType: "json",
+                contentType: "application/json; charset=UTF-8",
+                success: self.addedToQueue
+            }).always(function() {/*TODO: Remove the spinner once it exists*/});
+        }
+
+        self.addedToQueue = function(data) {
+            self.fromResponse(data);
+            self.editDialog.modal("hide");
+        }
+
+        self.showAddDialog = function() {
+            if (self.editDialog) {
+                var data
+                self.itemForEditing(new QueueItem());
+                self.addingNew(true);
+                self.editDialog.modal("show");
+            }
+        }
+
+        self.modifyQueueItem = function(event) {
+            var payload = {
+                id: self.itemForEditing().id(),
+                staff: self.itemForEditing().staff(),
+                customer: self.itemForEditing().customer(),
+                contact: self.itemForEditing().contact(),
+                filename: self.itemForEditing().filename(),
+                note: self.itemForEditing().note(),
+                cost: self.itemForEditing().cost(),
+                prepaid: self.itemForEditing().prepaid(),
+                printtype: self.printtypes().findIndex(x => x == self.itemForEditing().printtype())
+            };
+
+            $.ajax({
+                url: "plugin/queue/modifyitem",
+                type: "PUT",
+                data: JSON.stringify(payload),
+                dataType: "json",
+                contentType: "application/json; charset=UTF-8",
+                success: self.addedToQueue
+            }).always(function() {/*TODO: Remove the spinner once it exists*/});
+        }
+
+        self.showEditDialog = function(data) {
+            if (self.editDialog) {
+                self.itemForEditing(data);
+                self.addingNew(false);
+                self.editDialog.modal("show");
+            }
+        }
+
+        self.onCancelEdit = function() {
+            /* Force a re-fetch from the database to overwrite the local changes that we don't want to save. */
+            self.dataIsStale = true;
+            self.requestData();
+        }
+
+        self.onCancelArchive = function() {
+        }
+
+        
+        /* ### Settings ### */
+
+        self.addNewPrintType = function(name) {
+            self.printtypes.push(name);
+        };
+
+        self.removePrintType = function(printtype) {
+            self.printtypes.remove(printtype);
+        };
+
+        self.movePrintTypeUp = function(printtype) {
+            console.log("TODO: Move print type up.");
+        };
+
+        self.movePrintTypeDown = function(printtype) {
+            console.log("TODO: Move print type down.");
+        };
+
+        self.onSettingsBeforeSave = function () {
+            self.global_settings.settings.plugins.queue.printtypes(self.printtypes);
+        }
+
+        /* ItemListHelper is provided by Octoprint in helpers.js */
+        self.listHelper = new ItemListHelper(
+            "queueItems",
+            {
+                "typeThenDate": function (a,b) {
+                    if (a.printtype() < b.printtype()) {
+                        return -1;
+                    } else if (a.printtype() > b.printtype()) {
+                        return 1;
+                    } else {
+                        if (a.submissiontimestamp() < b.submissiontimestamp()) {
+                            return -1;
+                        } else if (a.submissiontimestamp() > b.submissiontimestamp()) {
+                            return 1;
+                        }
+                    }
+                    return 0;
+                },
+                "dateOnly": function (a,b) {
+                    if (a.submissiontimestamp() < b.submissiontimestamp()) {
+                        return -1;
+                    } else if (a.submissiontimestamp() > b.submissiontimestamp()) {
+                        return 1;
+                    }
+                    return 0;
+                }
+            },/*supportedSorting*/
+            {
+                "queue": function (item) {
+                    return (item.archived() == 0);
+                },
+                "archive": function (item) {
+                    return (item.archived() == 1);
+                }
+            },/*supportedFilters*/
+            "typeThenDate", /*defaultSorting*/
+            ["queue"], /*defaultFilter*/
+            [["queue","archive"]], /*exclusiveFilters*/
+            25 /*defaultPageSize*/ );
+
+        self.typeThenDateSort = function() {
+            self.listHelper.changeSorting("typeThenDate");
+        }
+
+        self.dateOnlySort = function() {
+            self.listHelper.changeSorting("dateOnly");
+        }
+
+    }
+
+    /* view model class, parameters for constructor, container to bind to
+     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
+     * and a full list of the available options.
+     */
+    OCTOPRINT_VIEWMODELS.push({
+        construct: QueueViewModel,
+        dependencies: [ "loginStateViewModel", "settingsViewModel", "usersViewModel" ],
+        elements: ["#tab_plugin_queue", "#settings_plugin_queue" ]
+    });
+});
